c28c59ee146eb69ac8f33193514a62595476a6c4
mcmillsd <mcmillsd@mcmaster.ca>
2015-11-18 13:38:37 -0500

    pages 1,2,3,4
    yea

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,5 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class UserInterface {
 	private Array readables;
@@ -23,10 +31,79 @@ public class UserInterface {
 	public void getReadables(){} // fetches all readables from the files and places them in the readables array
 	public void showAudioProducts(){} //Displays all audio products for browsing.
 	public void page1(){	
-		 
-		System.out.println("1. Sign in");
-		System.out.println("2. Sign up");
-		System.out.println("Choose your option");
+		Scanner a = new Scanner(System.in);
+		int p1choice;
+		System.out.println("1.Sign in");
+		System.out.println("2.Sign up");
+		System.out.print("Choose your option:");
+		s = a.nextInt();
+		if(p1choice==1){
+			changeCurrentPage(3);
+		}
+		else if(p1choice==2){
+			changeCurrentPage(2);
+		}
+	}
+	private static void page2(){
+		System.out.print("Choose your username:");
+		File file = new File("Users.txt");
+		Scanner a = new Scanner(System.in);
+		String p2choice = a.next();
+		FileWriter write;
+		if(isInFile(p2choice, "Users.txt")){
+			System.out.println("That username already exists");
+			changeCurrentPage(1);
+		}
+		else{
+			try {
+				write = new FileWriter(file, true);
+				BufferedWriter buffer = new BufferedWriter(write);
+				if (file.length() != 0){
+					buffer.newLine();
+					buffer.write(p2choice);
+				}else{
+					buffer.write(p2choice);
+				}
+				buffer.close();
+				System.out.println("Username successfully added");
+				changeCurrentPage(1);
+				}
+			catch (IOException e) {
+			e.printStackTrace();
+			}
+		}
+	}
+	private static void page3() {
+		Scanner a = new Scanner(System.in);
+		String p3choice;
+		System.out.print("Enter your username:");
+		p3choice = a.next();
+		if(isInFile(p3choice, "Users.txt")){
+			System.out.println("Hello " + p3choice);
+			changeCurrentPage(5);
+		}
+		else{
+			changeCurrentPage(4);
+		}
+	}
+	private static void page4(){
+		System.out.println("No Access");
+		changeCurrentPage(1);
+	}
+	private static boolean isInFile(String input, String infile){
+		File file = new File(infile);
+		Scanner reader = null;
+		try {
+			reader = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		List<String> list=new ArrayList<>();
+		while(reader.hasNextLine()){
+			list.add(reader.nextLine());
+		}
+		reader.close();
+		return list.contains(input);
 	}
 	
 }	
