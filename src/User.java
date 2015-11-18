11cbbad2214cd33333e11a56930ba88a65b0733c
robichne <neil.robichaud@hotmail.com>
2015-11-17 21:41:46 -0500

    Added ShoppingCart, User, and UserInterface java files

---
+++ src/User.java
@@ -0,0 +1,6 @@
+
+public class User{
+	private String username;
+	public String getUsername(...){} // stores the username.
+		return "hello";
+}
---
+++ src/UserInterface.java
@@ -0,0 +1,14 @@
+
+public class UserInterface {
+	private array readables;
+	private array audioProducts;
+	private int currentPage; // the page number (p1..p10)
+	public int getCurrentPage(..) {//This method is for page navigation. Based on the values of the state variable, call different pages.
+		
+	}
+	public int changeCurrentPage(..){//This method is for page navigation. It should change to current page and show the content.
+	
+	}
+	public void getReadables(); // fetches all readables from the files and places them in the readables array
+	public void showAudioProducts(); //Displays all audio products for browsing.
+}	
