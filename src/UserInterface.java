c476b2ecaab6c9d26b9768750a573446c61a8b40
robichne <neil.robichaud@hotmail.com>
2015-11-17 21:44:37 -0500

    changed brackets

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,7 +1,9 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class UserInterface {
-	private array readables;
-	private array audioProducts;
+	private Array readables;
+	private Array audioProducts;
 	private int currentPage; // the page number (p1..p10)
 	public int getCurrentPage(..) {//This method is for page navigation. Based on the values of the state variable, call different pages.
 		
@@ -9,6 +11,6 @@ public class UserInterface {
 	public int changeCurrentPage(..){//This method is for page navigation. It should change to current page and show the content.
 	
 	}
-	public void getReadables(); // fetches all readables from the files and places them in the readables array
-	public void showAudioProducts(); //Displays all audio products for browsing.
+	public void getReadables(){} // fetches all readables from the files and places them in the readables array
+	public void showAudioProducts(){} //Displays all audio products for browsing.
 }	
