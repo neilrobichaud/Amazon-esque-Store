1ce958ce7c1d1c0a003c672f6a8eef967083659f
robichne <neil.robichaud@hotmail.com>
2015-11-22 20:19:33 -0500

    Revert "df"
    This reverts commit 2524d4db0efd55030cbf68256c8167106d4883e1.

--- src/User.java
+++ src/User.java
@@ -3,17 +3,8 @@ public class User{
 	private static String username;	
 	public static String getUsername(){
 		return username;
-	}
-	private static boolean isAdmin=false;
-	
+	} 
 	public User(String name){
-		username=name;	//should add username to users.txt
-	}
-	public static boolean adminStatus(){	//getter for isAdmin
-		if (username.equals("ADMIN")){
-			isAdmin=true;
-		}
-		return isAdmin;
-		
+		username=name;
 	}
 }
