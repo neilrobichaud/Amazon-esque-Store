2524d4db0efd55030cbf68256c8167106d4883e1
robichne <neil.robichaud@hotmail.com>
2015-11-22 19:44:08 -0500

    df
    sdf

--- src/User.java
+++ src/User.java
@@ -3,8 +3,17 @@ public class User{
 	private static String username;	
 	public static String getUsername(){
 		return username;
-	} 
+	}
+	private static boolean isAdmin=false;
+	
 	public User(String name){
-		username=name;
+		username=name;	//should add username to users.txt
+	}
+	public static boolean adminStatus(){	//getter for isAdmin
+		if (username.equals("ADMIN")){
+			isAdmin=true;
+		}
+		return isAdmin;
+		
 	}
 }
